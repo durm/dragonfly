ced659a08d2ab41d5adceb359dcc28f46fcc4366
alex <akorotkov@mcfr.ru>
2015-10-12 19:26:32 +0600

    static settings

--- dragonfly/settings.py
+++ dragonfly/settings.py
@@ -108,3 +108,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_DIR = os.path.join(os.path.dirname(BASE_DIR), "data")
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")

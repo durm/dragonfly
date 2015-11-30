b151959abb61d70d635f348e72fb07e80059b96e
Alex Korotkov <durm.icecoffee@gmail.com>
2015-11-30 19:17:42 +0300

    login logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^login/$', 'accounts.views.login_endpoint', name='login_endpoint'),
     url(r'^me/$', 'accounts.views.me_endpoint', name='me_endpoint'),
+    url(r'^logout/$', 'accounts.views.logout_endpoint', name='logout_endpoint'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,17 +4,25 @@ from django.template.context_processors import csrf
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
 def login_endpoint(request):
     if request.user.is_authenticated():
         return redirect(reverse('me_endpoint'))
+    ctx = {}
+    ctx.update(csrf(request))
     if request.method == 'GET':
-        ctx = {}
-        ctx.update(csrf(request))
         return render_to_response("accounts/login.html", ctx, context_instance=RequestContext(request))
     elif request.method == 'POST':
-        print (111)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None and user.is_active:
+            login(request, user)
+            return redirect(reverse('me_endpoint'))
+        else:
+            ctx["error"] = "Incorrect login or password!"
+            return render_to_response("accounts/login.html", ctx, context_instance=RequestContext(request))
     else:
         raise Http404
 
@@ -22,3 +30,7 @@ def login_endpoint(request):
 def me_endpoint(request):
     ctx = {}
     return render_to_response("accounts/me.html", ctx, context_instance=RequestContext(request))
+    
+def logout_endpoint(request):
+    logout(request)
+    return redirect(reverse("login_endpoint"))
